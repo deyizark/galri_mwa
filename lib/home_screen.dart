459601cbc987bb31618f8426c3969c8db9a5da67
taylor12688 <alcerotaylor785@gmail.com>
2026-02-15 23:04:04 -0500

    deuxieme modification

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -10,13 +10,15 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final Set<int> favoritePhotos = {};
+
   int _currentIndex = 0;
 
   final List<Photographer> photographers = [
-    Photographer(photographerId: "1", photographer: "Jean Paul"),
-    Photographer(photographerId: "2", photographer: "Marie Claire"),
-    Photographer(photographerId: "3", photographer: "David Louis"),
-    Photographer(photographerId: "4", photographer: "Sophia Noel"),
+    Photographer(photographerId: 1, photographer: "Jean Paul"),
+    Photographer(photographerId: 2, photographer: "Marie Claire"),
+    Photographer(photographerId: 3, photographer: "David Louis"),
+    Photographer(photographerId: 4, photographer: "Sophia Noel"),
   ];
 
   @override
